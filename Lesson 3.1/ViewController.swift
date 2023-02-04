6773b30f7df13246406255b3c84d4c04ef789eb5
Mark Foksha <mark.foksha1813@gmail.com>
2023-02-04 13:20:26 +0200

    Download and show image after tapping on button Activity indicator is active all the time image is loading

--- Lesson 3.1/ViewController.swift
+++ Lesson 3.1/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var tapLabel: UILabel!
+    @IBOutlet weak var getImageViewButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        activityIndicator.isHidden = true
+        activityIndicator.hidesWhenStopped = true
+        
     }
-
+    
+    @IBAction func imageViewButtonPressed(_ sender: UIButton) {
+        tapLabel.isHidden = true
+        getImageViewButton.isEnabled = false
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        let urlString = "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg"
+        guard let url = URL(string: urlString) else { return }
+        
+        let session = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                let image = UIImage(data: data)
+                DispatchQueue.main.async {
+                    self.activityIndicator.stopAnimating()
+                    self.imageView.image = image
+                }
+            }
+        }
+        session.resume()
+    }
 
 }
 
